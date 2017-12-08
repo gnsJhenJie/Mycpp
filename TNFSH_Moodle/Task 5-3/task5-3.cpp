30f0526a25c46c3281b7197246267c90dc4c5394
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2017-12-08 22:19:14 +0800

    Create task5-3.cpp

---
+++ TNFSH_Moodle/Task 5-3/task5-3.cpp
@@ -0,0 +1,27 @@
+//Copyright 2017 gnsJhenJie.All right reserved.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n,tmp,tmpp,j=1;
+    cin>>n;
+    tmpp=n;
+    for (int i=1;i<=n;i++){
+        
+        for( j=1;j<=(n-i);j++){
+            cout<<0;
+            tmp=n-j;
+        }
+        if (j==1){
+            cout<<1;
+        }
+        for(int k=1;k<=tmp;k++){
+        
+            cout <<tmpp;
+        }
+        tmpp--;
+        cout<<"\n";
+        
+    }
+
+}
