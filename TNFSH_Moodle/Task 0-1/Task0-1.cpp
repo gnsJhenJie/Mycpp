e1f5697b3e571cb612c54620684730646f19a9c2
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2017-12-08 21:40:17 +0800

    Create Task0-1.cpp

---
+++ TNFSH_Moodle/Task 0-1/Task0-1.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  task2
+//
+//  Created by gnsJhenJie on 2017/9/20.
+//  Copyright gnsJhenJie. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    double r,h;
+    cin >> r >> h ;
+    cout << r*r*h*3.1416 ;
+    
+    
+    return 0;
+}
