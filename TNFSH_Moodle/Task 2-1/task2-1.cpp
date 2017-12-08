9c3c6fa39ce5e32a70d54dedd833022889085a88
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2017-12-08 21:46:52 +0800

    Create task2-1.cpp

---
+++ TNFSH_Moodle/Task 2-1/task2-1.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  20171011MaxMin
+//
+//  Created by Student on 2017/10/11.
+//  Copyright 穢 2017撟?HsJhenJie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    using namespace std;
+    double a , b , c ;
+    double avg;
+    cin >> a >> b >> c;
+    avg=(a+b+c)/3;
+    if (avg>100|avg<0){
+        cout << "BS" <<endl;
+        
+    }
+
+    else if (avg<60){
+        cout << "FAIL" <<endl;
+    }
+    else {
+        cout <<"PASS" <<endl;
+    }
+
+}
