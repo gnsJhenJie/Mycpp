663fb5b6af58fbdb8069ee64e3a646ec56d46705
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2017-12-08 21:41:47 +0800

    Update Task0.cpp

--- TNFSH_Moodle/Task 0/Task0.cpp
+++ TNFSH_Moodle/Task 0/Task0.cpp
@@ -1,21 +1,19 @@
-    1 //
-    2 //  main.cpp
-    3 //  Task0
-    4 //
-    5 //  Created by Student on 2017/9/20.
-    6 //  Copyright © 2017年 TNFSH. All rights reserved.
-    7 //
-    8 
-    9 #include <iostream>
-   10 using namespace std;
-   11 
-   12 int main(int argc, const char * argv[]) {
-   13     // insert code here...
-   14     int a,b;
-   15     cin >> a >> b ;
-   16     cout << (a+b)/2  ;
-   17 
-   18     
-   19     return 0;
-   20 }
-   21 
+//
+//  main.cpp
+//  task0
+//
+//  Created by gnsJhenJie on 2017/9/20.
+//  Copyright gnsJhenJie. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    double r,h;
+    cin >> r >> h ;
+    cout << r*r*h*3.1416 ;
+    
+    
+    return 0;
+}
