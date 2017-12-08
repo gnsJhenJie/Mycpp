ab8293cc3a68217c703db892e56004dc37837f14
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2017-12-08 22:00:15 +0800

    Create task2-5.cpp

---
+++ TNFSH_Moodle/Task 4-5/task2-5.cpp
@@ -0,0 +1,21 @@
+//Copyright 2017 gnsJhenJie
+//Task 2-5
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    using namespace  std;
+long long int a,i;
+ cin >> a;
+i=2;
+for (i=2 ; i<a ; i++ ) {
+    if ( a % i == 0 ) {
+        cout << i << endl;
+
+    }
+}
+return 0 ;
+}
