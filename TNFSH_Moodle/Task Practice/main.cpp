5d679a1e3654dcdd52e65061993a4d0684d6742d
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2018-02-11 20:37:00 +0800

    Create main.cpp

---
+++ TNFSH_Moodle/Task Practice/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cout<<"I asked my wife, Where do you want to go on our anniversary?\nShe said, Somewhere I have never been!\nI told her, How about the kitchen?\n";
+    return 0;
+}
