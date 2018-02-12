9e5c57dfd2c65e253b16de179157a5e90b41c72e
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2018-02-12 10:37:53 +0800

    Update main.cpp

--- TNFSH_Moodle/Task 12-3/main.cpp
+++ TNFSH_Moodle/Task 12-3/main.cpp
@@ -33,41 +33,6 @@ int main(int argc, char** argv) {
 		sumtemp = 0;
 
 	}
-	//sort(by score)
-	/*bool success = 0;
-	int temp;
-	while (!success) {
-		success = 1;
-		for (int person = 0; person < pamt; person++) {
-			if (score[person][2] < score[person + 1][2]) {
-				for (int j = 0; j < samt; j++) {
-					temp = score[person][j];
-					score[person][j] = score[person + 1][j];
-					score[person + 1][j] = temp;
-				}
-				success = 0;
-			}
-		}
-	}
-	//input rank to array
-	for (int i = 0; i < pamt; i++) {
-		score[i][3] = i + 1;
-	}
-	//sort array to the begining type
-	success = 0;
-	while (!success) {
-		success = 1;
-		for (int person = 0; person < pamt; person++) {
-			if ((score[person][0] > score[person + 1][0])&&(person<pamt-1)) {
-				for (int j = 0; j < samt; j++) {
-					temp = score[person][j];
-					score[person][j] = score[person + 1][j];
-					score[person + 1][j] = temp;
-				}
-				success = 0;
-			}
-		}
-	}*/
 	//Output everyone's average
 	for (int person = 0; person < pamt; person++) {
 		printf("%.2f ", score[person][samt]);
